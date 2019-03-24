aff5b2a44ae722e65f02ee89e2d557227741bcae
unknown <ksjones@udel.edu>
2019-03-24 14:12:48 -0700

    Adding cards to deck with quanity. Untested.

--- client/src/actions/deckActions.js
+++ client/src/actions/deckActions.js
@@ -10,25 +10,34 @@ export const addToDeck = (cardToAdd) => (dispatch, getState) => {
 	// 	}
 	// }
 
+	// Check if card exists in deck
+	let quantity = 1;
 	const matchedCards = getState().deckBuilder.deck.deckCards.filter((entry) => entry.card.id === cardToAdd.id);
+
+	// Should only be 0 or 1, although might not be necessary
 	if (matchedCards.length > 1) {
 		console.log('ERROR: Found more than one of a card in the deck. ');
 		return;
 	}
 
 	if (matchedCards.length == 1) {
+		// Check if already 2 cards, or 1 for legendary
 		const deckCard = matchedCards[0];
-		if (deckCard !== null) {
-			if (deckCard.quanity >= 2 || (deckCard.card.rarity == 'LEGENDARY' && deckCard.quantity >= 1)) {
-				console.log('quantity max reached');
-				return;
-			}
+		if (deckCard.quanity >= 2 || (deckCard.card.rarity == 'LEGENDARY' && deckCard.quantity >= 1)) {
+			console.log('quantity max reached');
+			return;
 		}
+
+		// Set quantity to 2 if card already exists in deck
+		quantity = 2;
 	}
 
 	dispatch({
 		type    : ADD_TO_DECK,
-		payload : cardToAdd
+		payload : {
+			card     : cardToAdd,
+			quantity
+		}
 	});
 };
 
