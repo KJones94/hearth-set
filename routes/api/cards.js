8797980be49510c67d311ee090080e1eb02becf0
unknown <ksjones@udel.edu>
2019-05-22 09:57:38 -0700

    Limiting card data returned to only what is used.

--- routes/api/cards.js
+++ routes/api/cards.js
@@ -4,11 +4,30 @@ const router = express.Router();
 // Card model
 const Card = require('../../models/Card');
 
+const valuesIncluded = {
+	// Only a few keys aren't included, so might not be needed
+	attack         : 1,
+	cardClass      : 1,
+	cost           : 1,
+	durability     : 1,
+	elite          : 1,
+	health         : 1,
+	id             : 1,
+	mechanics      : 1,
+	name           : 1,
+	race           : 1,
+	rarity         : 1,
+	referencedTags : 1,
+	set            : 1,
+	text           : 1,
+	type           : 1
+};
+
 // @route GET api/cards
 // @desc Get All Cards
 // @access Public
 router.get('/', (req, res) => {
-	Card.find().sort({ id: 1 }).then((cards) => res.json(cards));
+	Card.find({}, valuesIncluded).sort({ id: 1 }).then((cards) => res.json(cards));
 });
 
 // @route POST api/cards
@@ -17,7 +36,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 	Card.find(req.body).then((cards) => {
 		res.json(cards);
-	});
+	}, valuesIncluded);
 });
 
 module.exports = router;
